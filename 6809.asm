0af7547e31117669b434287e0bea5e1c1fd25ae3
John Metcalf <digital.wilderness@googlemail.com>
2022-09-25 14:47:04 +0100

    Update 6809.asm

--- 6809.asm
+++ 6809.asm
@@ -1,3 +1,12 @@
+; 16-bit xorshift 6809 pseudorandom number generator by John Metcalf
+
+; generates 16-bit pseudorandom numbers with a period of 65535
+; using the xorshift method
+
+; XSHFT ^= XSHFT << 7
+; XSHFT ^= XSHFT >> 9
+; XSHFT ^= XSHFT << 8
+
   LDD XSHFT
   RORA
   RORB
